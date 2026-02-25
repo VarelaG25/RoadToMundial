cdd41d09a41ed3a9e00191948de6cdce4c9006b2
VarelaG25 <vjosemario38@gmail.com>
2026-02-24 23:13:06 -0600

    configuracion del router

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,24 @@
-import { createWebHistory, createRouter } from "vue-router";
-import Home from "@/views/home/HomePage.vue";
-import Login from "@/views/login/LoginPage.vue";
+import { createWebHistory, createRouter } from 'vue-router'
+import Home from '@/views/home/HomePage.vue'
+import Login from '@/views/login/LoginPage.vue'
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home,
-    },
-    {
-        path: "/login",
-        name: "Login",
-        component: Login,
-    }
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
+  // import.meta.env.BASE_URL leerá el 'base' que pusiste en vite.config.ts
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
 
-export default router;
+export default router
